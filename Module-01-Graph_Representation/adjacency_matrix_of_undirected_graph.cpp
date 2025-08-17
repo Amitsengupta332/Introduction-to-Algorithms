a421727cc37acbfa3cd4723cded55f9e9f6b74bb
amitsengupta <amitsengupta332@gmail.com>
2025-08-17 17:25:29 +0600

    adjacency undirected graph

--- Module-01-Graph_Representation/adjacency_matrix_of_undirected_graph.cpp
+++ Module-01-Graph_Representation/adjacency_matrix_of_undirected_graph.cpp
@@ -6,12 +6,45 @@ int main()
 
     int n, e;
     cin >> n >> e;
+    int adj_mat[n][n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         adj_mat[i][j] = 0;
+    //     }
+    // }
+    memset(adj_mat, 0, sizeof(adj_mat));
+    for (int i = 0; i < n; i++)
+    {
+         for (int j = 0; j < n; j++)
+         {
+             if (i == j)
+             {
+                 adj_mat[i][j] = 1;
+             }
+         }
+         
+    }
+    
+
     for (int i = 0; i < e; i++)
     {
         int a, b;
         cin >> a >> b;
-        cout << a << " " << b << endl;
+        adj_mat[a][b] = 1;
+        adj_mat[b][a] = 1; // undirected
+        // cout << a << " " << b << endl;
     }
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
---
+++ Module-01-Graph_Representation/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+   for (int i = 0; i < n; i++)
+    {
+         for (int j = 0; j < n; i++)
+         {
+             if (i == j)
+             {
+                 adj_mat[i][j] = 1;
+             }
+         }
+         
+    }
