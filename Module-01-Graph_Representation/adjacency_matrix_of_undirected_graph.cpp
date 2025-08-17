48c89f500ddfa1d304007b6530a6d6e96cb78de5
amitsengupta <amitsengupta332@gmail.com>
2025-08-17 16:38:18 +0600

    f created for undirected graph

---
+++ Module-01-Graph_Representation/adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n, e;
+    cin >> n >> e;
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a << " " << b << endl;
+    }
+    
+    return 0;
+}
