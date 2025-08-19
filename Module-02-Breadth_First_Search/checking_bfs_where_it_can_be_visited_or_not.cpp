5189b52f3d6c2f2d47e411a74f14fbdbd0863680
amitsengupta <amitsengupta332@gmail.com>
2025-08-19 15:00:01 +0600

    checking bfs

---
+++ Module-02-Breadth_First_Search/checking_bfs_where_it_can_be_visited_or_not.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1005];
+bool vis[1005];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        // cout << par << " ";
+        for (int child : adj_list[par])
+        {
+            if (!vis[child])
+            {
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    int src, des;
+    cin >> src >> des;
+    bfs(src);
+
+    if (vis[des])
+    {
+        cout << "yes" << endl;
+    }
+    else
+    {
+        cout << "no" << endl;
+    }
+    return 0;
+}
