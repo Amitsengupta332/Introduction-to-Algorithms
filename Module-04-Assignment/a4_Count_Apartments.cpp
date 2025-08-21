b503e498f293368231de68461266885f2eddc216
amitsengupta <amitsengupta332@gmail.com>
2025-08-22 02:13:30 +0600

    done a4

--- Module-04-Assignment/a4_Count_Apartments.cpp
+++ Module-04-Assignment/a4_Count_Apartments.cpp
@@ -13,10 +13,46 @@ bool valid(int i, int j)
     return true;
 }
 
-
+void dfs(int si, int sj)
+{
+    vis[si][sj] = true;
+    for (int k = 0; k < 4; k++)
+    {
+        int ci = si + d[k].first;
+        int cj = sj + d[k].second;
+        if (valid(ci, cj) && grid[ci][cj] == '.' && !vis[ci][cj])
+        {
+            dfs(ci, cj);
+        }
+    }
+}
 
 int main()
 {
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+
+    memset(vis, false, sizeof(vis));
+    int apartments = 0;
+
+       for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == '.' && !vis[i][j]) {
+                apartments++;
+                dfs(i, j);
+            }
+        }
+    }
+
+    cout << apartments << endl;
 
     return 0;
 }
