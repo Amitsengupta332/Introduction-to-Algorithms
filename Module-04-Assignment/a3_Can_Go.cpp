5189e43edeb0832f3b1143b4856cc45c0329142a
amitsengupta <amitsengupta332@gmail.com>
2025-08-22 01:22:24 +0600

    done a3 in can go

--- Module-04-Assignment/a3_Can_Go.cpp
+++ Module-04-Assignment/a3_Can_Go.cpp
@@ -1,8 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
+char grid[1005][1005];
+bool vis[1005][1005];
+int level[1005][1005];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || j < 0 || i >= n || j >= m)
+        return false;
+    return true;
+}
+
+void bfs(int si, int sj)
+{
+    queue<pair<int, int>> q;
+    q.push({si, sj});
+    vis[si][sj] = true;
+    level[si][sj] = 0;
+
+    while (!q.empty())
+    {
+        auto [par_i, par_j] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = par_i + d[i].first;
+            int cj = par_j + d[i].second;
+
+            if (valid(ci, cj) && !vis[ci][cj] &&
+                (grid[ci][cj] == '.' || grid[ci][cj] == 'B'))
+            {
+                q.push({ci, cj});
+                vis[ci][cj] = true;
+                level[ci][cj] = level[par_i][par_j] + 1;
+            }
+        }
+    }
+}
 
 int main()
 {
-  
+
+    cin >> n >> m;
+
+    int si, sj, di, dj;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> grid[i][j];
+            if (grid[i][j] == 'A')
+            {
+                si = i, sj = j;
+            }
+            if (grid[i][j] == 'B')
+            {
+                di = i, dj = j;
+            }
+        }
+    }
+
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+
+    bfs(si, sj);
+
+    if (vis[di][dj])
+        cout << "YES\n";
+    else
+        cout << "NO\n";
     return 0;
 }
