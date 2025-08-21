fcaa35e4a267c14a0c6a53a4b79094d4facfc24a
amitsengupta <amitsengupta332@gmail.com>
2025-08-22 00:02:11 +0600

    done a1

--- Module-04-Assignment/a1_Connected_or_Not.cpp
+++ Module-04-Assignment/a1_Connected_or_Not.cpp
@@ -4,5 +4,30 @@ using namespace std;
 int main()
 {
   
+    int n, e;
+    cin >> n >> e;
+
+    set<pair<int, int>> edges;
+
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        edges.insert({u, v});
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == b || edges.count({a, b}))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
     return 0;
 }
