1536507bf4975bdcad612b465eb2d1a951623acc
amitsengupta <amitsengupta332@gmail.com>
2025-08-25 21:07:57 +0600

    detect cycle in undirected in bfs

--- Module-06-Cycle_Detection/detect_cycle_in_undirected_graph_in_bfs.cpp
+++ Module-06-Cycle_Detection/detect_cycle_in_undirected_graph_in_bfs.cpp
@@ -1,8 +1,93 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool vis[105];
+vector<int> adj_list[105];
+int parent[105];
+bool cycle ;
+
+// bool bfs(int src)
+// {
+//     queue<int> q;
+//     q.push(src);
+//     vis[src] = true;
+//     while (!q.empty())
+//     {
+//         int par = q.front();
+//         q.pop();
+//         for (int child : adj_list[par])
+//         {
+//             if(vis[child] && parent[par] != child)
+//             {
+//                 cycle = true;
+//                 // return true;
+//             }
+//             if (!vis[child])
+//             {
+//                 vis[child] = true;
+//                 q.push(child);
+//                 parent[child] = par;
+//             }
+//         }
+//     }
+// }
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        for (int child : adj_list[par])
+        {
+            if (vis[child] && parent[par] != child)
+            {
+                cycle = true;
+            }
+            if (!vis[child])
+            {
+                vis[child] = true;
+                q.push(child);
+                parent[child] = par;
+            }
+        }
+    }
+}
 
 int main()
 {
-  
+    int n, e;
+    cin >> n >> e;
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(vis, false, sizeof(vis));
+    memset(parent, -1, sizeof(parent));
+    cycle = false;
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            bfs(i);
+        }
+    }
+
+    if (cycle)
+    {
+        cout << "Cycle is present" << endl;
+    }
+    else
+    {
+        cout << "Cycle is not present" << endl;
+    }
+
     return 0;
 }
