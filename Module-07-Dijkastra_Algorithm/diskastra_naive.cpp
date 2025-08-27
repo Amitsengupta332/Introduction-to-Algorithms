18b1e20943c30660e74dab0a653d176bfe51712d
amitsengupta <amitsengupta332@gmail.com>
2025-08-27 23:12:10 +0600

    done diskastra

---
+++ Module-07-Dijkastra_Algorithm/diskastra_naive.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int, int>> adj_list[105];
+int dist[105];
+void dijkastra(int src)
+{
+    queue<pair<int, int>> q;
+    q.push({src, 0});
+    dist[src] = 0;
+
+    while (!q.empty())
+    {
+
+        pair<int, int> par = q.front();
+        q.pop();
+        int par_node = par.first;
+        int par_dist = par.second;
+
+        // children k push kora
+        for (auto child : adj_list[par_node])
+        {
+            int child_node = child.first;
+            int child_dist = child.second;
+            if (par_dist + child_dist < dist[child_node])
+            {
+                dist[child_node] = par_dist + child_dist;
+                q.push({ child_node, dist[child_node] });
+            }
+        }
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        adj_list[a].push_back({b, w});
+        adj_list[b].push_back({a, w});
+    }
+    for (int i = 0; i < n; i++)
+        dist[i] = INT_MAX;
+
+    dijkastra(0);
+    for (int i = 0; i < n; i++)
+        cout << i << " -> " << dist[i] << endl;
+    // memset(dist, INT_MAX, sizeof(dist));
+
+    return 0;
+}
