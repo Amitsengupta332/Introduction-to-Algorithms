38b7ac57ba4e2d23f24adac30d411a7fc4855343
amitsengupta <amitsengupta332@gmail.com>
2025-09-08 15:45:31 +0600

    fibonacci

--- Module-14-Dynamic_Programming/factorial.cpp
+++ Module-14-Dynamic_Programming/factorial.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fact(int n)
+int fibo(int n)
 {
     if (n == 1)
         return 1;
-    int mul = fact(n - 1);
+    int mul = fibo(n - 1);
     return n * mul;
 }
 
 int main()
 {
-    cout << fact(5) << endl;
+    cout << fibo(5) << endl;
     return 0;
 }
