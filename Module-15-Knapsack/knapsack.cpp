5e1b9fe455a9c3d362f7e1d134d8354d9ec0e666
amitsengupta <amitsengupta332@gmail.com>
2025-09-10 15:31:16 +0600

    knapsace

--- Module-15-Knapsack/knapsack.cpp
+++ Module-15-Knapsack/knapsack.cpp
@@ -1,24 +1,83 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int val[1005], weight[1005];
+
+// int knapsack(int i, int mx_weight)
+// {
+//     // 2 options
+//     // bag e rakhbo
+//     // bag e rakhbo na
+//     if (i < 0 || mx_weight <= 0)
+//     {
+//         return 0;
+//     }
+
+//     if (weight[i] <= mx_weight)
+//     {
+//         int op1 = knapsack(i - 1, mx_weight - weight[i]);
+//         int op2 = knapsack(i - 1, mx_weight);
+
+//         return max(op1, op2);
+//     }
+//     else
+//     {
+//         return knapsack(i - 1, mx_weight);
+//     }
+// }
+// int main()
+// {
+//     int n, mx_weight;
+//     cin >> n;
+//     // int val[n], weight[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> val[i];
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> weight[i];
+//     }
+
+//     cin >> mx_weight;
+
+//     cout << knapsack(n - 1, mx_weight) << endl;
+
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
-int val[1005], weight[1005];
 
+int val[1005], weight[1005];
 
 int knapsack(int i, int mx_weight)
 {
-    // 2 options
-    // bag e rakhbo 
-    // bag e rakhbo na
-    int op1= knapsack(i-1, mx_weight-weight[i]);
-    int op2= knapsack(i-1, mx_weight);
+    if (i < 0 || mx_weight <= 0)
+    {
+        return 0;
+    }
 
-    return max(op1, op2);
+    if (weight[i] <= mx_weight)
+    {
+        // Option 1: take item i
+        int op1 = val[i] + knapsack(i - 1, mx_weight - weight[i]);
+        // Option 2: skip item i
+        int op2 = knapsack(i - 1, mx_weight);
 
+        return max(op1, op2);
+    }
+    else
+    {
+        return knapsack(i - 1, mx_weight);
+    }
 }
+
 int main()
 {
     int n, mx_weight;
     cin >> n;
-    int val[n], weight[n];
 
     for (int i = 0; i < n; i++)
     {
@@ -31,7 +90,7 @@ int main()
 
     cin >> mx_weight;
 
-    knapsack(n-1, mx_weight);
+    cout << knapsack(n - 1, mx_weight) << endl;
 
     return 0;
 }
