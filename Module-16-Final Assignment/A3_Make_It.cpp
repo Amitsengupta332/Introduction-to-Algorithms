82052af6e8857333c6f97f61ce2625d7a48f599b
amitsengupta <amitsengupta332@gmail.com>
2025-09-12 15:33:53 +0600

    done make it assignment 3

--- Module-16-Final Assignment/A3_Make_It.cpp
+++ Module-16-Final Assignment/A3_Make_It.cpp
@@ -1,8 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool reachable(int n)
+{
+    queue<int> q;
+    vector<bool> visited(n + 1, false);
+    
+    q.push(1);
+    visited[1] = true;
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        if (cur == n) return true;
+
+        // Next moves
+        long long add = cur + 3;
+        long long mul = cur * 2;
+
+        if (add <= n && !visited[add]) {
+            visited[add] = true;
+            q.push(add);
+        }
+        if (mul <= n && !visited[mul]) {
+            visited[mul] = true;
+            q.push(mul);
+        }
+    }
+    return false;
+}
+
 int main()
 {
-  
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (reachable(n))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
     return 0;
 }
